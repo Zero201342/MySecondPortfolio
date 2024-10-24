6b7ec9f5be54b3753a735de54fbaf3329cacc778
Zero201342 <tulenhtaungam2025@gmail.com>
2024-10-25 03:25:20 +0700

    Update baitap2.js
    chặn source

--- upgraded-portfolio-WHYNOT/baitap2.js
+++ upgraded-portfolio-WHYNOT/baitap2.js
@@ -23,3 +23,28 @@ document.querySelector('.home-icon').addEventListener('click', (event) => {
     event.preventDefault(); 
     window.location.href = 'test1.html'; 
 });
+
+// Chặn F12
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'F12') {
+        event.preventDefault();
+    }
+});
+
+// Chặn Ctrl+U
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.key === 'u') {
+        event.preventDefault();
+    }
+});
+
+// Chặn Ctrl+Shift+I (mở DevTools)
+document.addEventListener('keydown', function (event) {
+    if (event.ctrlKey && event.shiftKey && event.key === 'I') {
+        event.preventDefault();
+    }
+});
+
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
